e407d1dfbcf70b15b475f338c2fce2a524c53092
Ragul <ragulonline4@gmail.com>
2024-12-07 02:49:24 +0530

    2

--- Client/engine.js
+++ Client/engine.js
@@ -129,41 +129,108 @@ remote.addEventListener('wheel', (e) => {
 window.addEventListener('keydown', (e) => {
     e.preventDefault();
     if(e.ctrlKey || e.altKey || e.shiftKey){
-        // Shortcut keys
-        if (e.ctrlKey && e.key === 'c' || e.ctrlKey && e.key === 'C') {
+        // Ctrl Shortcut keys
+        if (e.ctrlKey && e.key.toLowerCase() === 'c') {
             channel.send(JSON.stringify({type: 'short-key', key: 'copy'}))
             console.log(JSON.stringify({type: 'short-key', key: 'copy'}))
-        } else if (e.ctrlKey && e.key === 'x' || e.ctrlKey && e.key === 'X') {
+        } 
+        else if (e.ctrlKey && e.key.toLowerCase() === 'x') {
             channel.send(JSON.stringify({type: 'short-key', key: 'cut'}))
             console.log(JSON.stringify({type: 'short-key', key: 'cut'}))
-        } else if (e.ctrlKey && e.key === 'v' || e.ctrlKey && e.key === 'V') {
+        } 
+        else if (e.ctrlKey && e.key.toLowerCase() === 'v') {
             channel.send(JSON.stringify({type: 'short-key', key: 'paste'}))
             console.log(JSON.stringify({type: 'short-key', key: 'paste'}))
         }
-        else if (e.ctrlKey && e.key === 'z' || e.ctrlKey && e.key === 'Z') {
+        else if (e.ctrlKey && e.key.toLowerCase() === 'z') {
             channel.send(JSON.stringify({type: 'short-key', key: 'undo'}))
             console.log(JSON.stringify({type: 'short-key', key: 'undo'}))
         }
-        else if (e.ctrlKey && e.key === 's' || e.ctrlKey && e.key === 'S') {
+        else if (e.ctrlKey && e.key.toLowerCase() === 's') {
             channel.send(JSON.stringify({type: 'short-key', key: 'save'}))
             console.log(JSON.stringify({type: 'short-key', key: 'save'}))
         }
-        else if (e.ctrlKey && e.key === 'a' || e.ctrlKey && e.key === 'A') {
+        else if (e.ctrlKey && e.key.toLowerCase() === 'a') {
             channel.send(JSON.stringify({type: 'short-key', key: 'all'}))
             console.log(JSON.stringify({type: 'short-key', key: 'all'}))
         }
-        else if (e.ctrlKey && e.key === 'y' || e.ctrlKey && e.key === 'Y') {
+        else if (e.ctrlKey && e.key.toLowerCase() === 'y') {
             channel.send(JSON.stringify({type: 'short-key', key: 'redo'}))
             console.log(JSON.stringify({type: 'short-key', key: 'redo'}))
         }
-        else if (e.ctrlKey && e.key === 'd' || e.ctrlKey && e.key === 'D') {
+        else if (e.ctrlKey && e.key.toLowerCase() === 'd') {
             channel.send(JSON.stringify({type: 'short-key', key: 'delete'}))
             console.log(JSON.stringify({type: 'short-key', key: 'delete'}))
+        }
+        else if (e.ctrlKey && e.key.toLowerCase() === 'p') {
+            channel.send(JSON.stringify({type: 'short-key', key: 'print'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'print'}))
+        }
+        else if (e.ctrlKey && e.key.toLowerCase() === 'r') {
+            channel.send(JSON.stringify({type: 'short-key', key: 'reload'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'reload'}))
         }
         // Captial keys (shfit + key)
         else if (e.shiftKey && e.key.length == 1){
             channel.send(JSON.stringify({type: 'key-stroke', key: e.key}))
             console.log(JSON.stringify({type: 'key-stroke', key: e.key}))
+        }
+        // Alt Shortcut keys
+        else if (e.altKey && e.key.toLowerCase() === 'tab'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'switch'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'switch'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'escape'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'cycle'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'cycle'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === " "){
+            channel.send(JSON.stringify({type: 'short-key', key: 'space'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'space'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'enter'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'props'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'props'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'arrowleft'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'prev'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'prev'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'arrowright'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'forw'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'forw'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'd'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'focus'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'focus'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'e'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'edit'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'edit'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'f'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'file'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'file'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'h'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'help'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'help'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'i'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'insert'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'insert'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'j'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'ribbon'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'ribbon'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'm'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'mail'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'mail'}))
+        }
+        else if (e.altKey && e.key.toLowerCase() === 'n'){
+            channel.send(JSON.stringify({type: 'short-key', key: 'new'}))
+            console.log(JSON.stringify({type: 'short-key', key: 'new'}))
         }
     }
     else{
@@ -207,8 +274,23 @@ window.addEventListener('beforeunload', (e) => {
 /*
 Ctrl + Shift + Esc: Open Task Manager directly.
 Ctrl + Alt + Del: Open the Security options screen (Task Manager, Lock, Log Off, etc.).
-Alt + Tab: Switch between open applications.
-Alt + F4: Close the currently focused window or application.
+Alt: Open the Start Menu (Windows 8 and later).
+Alt + F4: Close the currently active window or program.
+Shift + F10: Open the context menu (like right-clicking) for the selected item.
+Shift + Delete: Delete a selected file or folder without moving it to the Recycle Bin.
+Shift + Tab: Move backward between selectable items (opposite of Tab).
+Shift + Arrow Keys: Select text or items in the direction of the arrow key.
+Shift + Esc: Open Task Manager directly without going through Ctrl+Shift+Esc.
+Shift + F3: Toggle case between uppercase, lowercase, and title case in Microsoft Word.
+Shift + Enter: Perform a "soft" return (start a new line without starting a new paragraph) in some applications.
+Shift + Right-click: Open an extended context menu with more options in the right-click menu.
+Shift + Click: Select a range of items, files, or text between the first and last clicked items.
+Shift + Ctrl + Arrow Keys: Select text or items in larger increments (e.g., one word at a time).
+Shift + F6: Cycle through different panes or windows in a program (commonly in browsers or editors).
+Shift + Win + S: Open the Snipping Tool to take a screenshot (Windows 10/11).
+Shift + Win + Left or Right Arrow: Move an active window between multiple monitors in a multi-monitor setup.
+Shift + Win + M: Restore minimized windows on the desktop (Windows 10/11).
+Shift + Alt + Tab: Switch between open applications in reverse order (opposite of Alt + Tab)
 */
 
 /* Capture Mouse Mouse Click */
--- Controller/keyboard.py
+++ Controller/keyboard.py
@@ -26,5 +26,37 @@ def shortcut(cmd):
         pyautogui.hotkey('ctrl', 'y')
     elif cmd == 'delete':
         pyautogui.hotkey('ctrl', 'd')
+    elif cmd == 'print':
+        pyautogui.hotkey('ctrl', 'p')
+    elif cmd == 'reload':
+        pyautogui.hotkey('ctrl', 'r')
     elif cmd == 'close':
         pyautogui.hotkey('ctrl', 'f4')
+    elif cmd == 'switch':
+        pyautogui.hotkey('alt', 'tab')
+    elif cmd == 'cycle':
+        pyautogui.hotkey('alt', 'escape')
+    elif cmd == 'space':
+        pyautogui.hotkey('alt', 'space')
+    elif cmd == 'props':
+        pyautogui.hotkey('alt', 'enter')
+    elif cmd == 'prev':
+        pyautogui.hotkey('alt', 'left')
+    elif cmd == 'forw':
+        pyautogui.hotkey('alt', 'right')
+    elif cmd == 'focus':
+        pyautogui.hotkey('alt', 'd')
+    elif cmd == 'edit':
+        pyautogui.hotkey('alt', 'e')
+    elif cmd == 'file':
+        pyautogui.hotkey('alt', 'f')
+    elif cmd == 'help':
+        pyautogui.hotkey('alt', 'h')
+    elif cmd == 'insert':
+        pyautogui.hotkey('alt', 'i')
+    elif cmd == 'ribbon':
+        pyautogui.hotkey('alt', 'j')
+    elif cmd == 'mail':
+        pyautogui.hotkey('alt', 'm')
+    elif cmd == 'new':
+        pyautogui.hotkey('alt', 'n')
